1f78a0e66dca57cc39c4645ae1d45f22bbb364bb
Bastien Nocera <hadess@hadess.net>
2026-04-17 23:37:08 +0200

    Add io.github.Axel051171.UnifiedFloppyTool

---
+++ local-build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+rm -rf _build ; mkdir _build
+rm -rf _repo ; mkdir _repo
+
+BRANCH=test
+
+powerprofilesctl launch flatpak-builder -v --ccache --force-clean --default-branch=$BRANCH _build io.github.Axel051171.UnifiedFloppyTool.yml --repo=_repo
+flatpak build-bundle _repo io.github.Axel051171.UnifiedFloppyTool.flatpak io.github.Axel051171.UnifiedFloppyTool $BRANCH

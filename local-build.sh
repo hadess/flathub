250e5bd430130493d5aec6cc72519ba2250cbd59
Bastien Nocera <hadess@hadess.net>
2026-04-19 14:10:16 +0200

    Update UFT to remove upstreamed patches

--- local-build.sh
+++ local-build.sh
@@ -5,5 +5,5 @@ rm -rf _repo ; mkdir _repo
 
 BRANCH=test
 
-powerprofilesctl launch flatpak-builder -v --ccache --force-clean --default-branch=$BRANCH _build io.github.Axel051171.UnifiedFloppyTool.yml --repo=_repo
+powerprofilesctl launch flatpak-builder --ccache --force-clean --default-branch=$BRANCH _build io.github.Axel051171.UnifiedFloppyTool.yml --repo=_repo
 flatpak build-bundle _repo io.github.Axel051171.UnifiedFloppyTool.flatpak io.github.Axel051171.UnifiedFloppyTool $BRANCH
